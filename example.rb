f50fc45b25f5669bda9a9cfa3940ab5f29814b35
Alex Martin <martin.alex.t@gmail.com>
2020-12-07 13:48:17 -0500

    Modules & extra methods

--- example.rb
+++ example.rb
@@ -1,21 +1,38 @@
+require 'date'
+
 class Animal
     def speak(name)
         "My name is #{name}!"
     end
 end
 
-class Dog < Animal
-    attr_accessor :name
-
-    def initialize(name)
-        @name = name
-    end
-
-    def speak
-        "Woof! #{super(name)}"
+module Age
+    def age(birthdate)
+        current_year = Date.today.year
+        current_year - Date.parse(birthdate).year
     end
 end
 
-dog = Dog.new("Mia")
+class Dog < Animal
+    include Age
+
+    attr_accessor :name, :breed, :birthday
+
+    def initialize(name:, breed:, birthday:)
+        @name = name
+        @breed = breed
+        @birthday = birthday
+    end
+
+    def speak
+        "Woof! #{super(name)}! I am #{age(birthday)} years old!"
+    end
+
+    def self.names(dog_house)
+        dog_house.map { |dog| dog.name }
+    end
+end
+
+dog = Dog.new(breed: "border collie", birthday: "04/06/2006", name: "Mia")
 puts dog.speak
 puts dog.name
