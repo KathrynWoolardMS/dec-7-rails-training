433a3473ff71a2b4b23275fae9001484b140b717
Jamie Phelps <jamie@jamiephelps.com>
2020-12-08 09:46:34 -0800

    Adding movies route, controller, and view

--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class MoviesSystemTest < ApplicationSystemTestCase
-  test "visiting the movie show" do
+  test "visiting movie 1" do
   #     As a user,
   # when I visit /movies/1
     visit "/movies/1"
@@ -9,5 +9,11 @@ class MoviesSystemTest < ApplicationSystemTestCase
     assert_text "Parasite"
     # I also see the name of the director "Bong Joon-ho"
     assert_text "Bong Joon-ho"
+  end
+
+  test "visiting movie 2" do
+    # As a user
+    # When I visit a movie with the id 2
+    # I see the title for that movie
   end
 end
